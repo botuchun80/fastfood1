1e0cb3f0b10b5843d8fdf7b4d45d6caf7c366994
botuchun80 <botuchun80@gmail.com>
2025-12-04 19:56:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const items = [
-  {id:1,img:"https://i.ibb.co/sJtWCn5M/images-1.jpg",name:"Burger",price:50000},
-  {id:2,img:"https://i.ibb.co/sJtWCn5M/images-1.jpg",name:"Cheeseburger",price:42000},
-  {id:3,img:"https://i.ibb.co/sJtWCn5M/images-1.jpg",name:"Hamburger",price:45000},
-  {id:4,img:"https://i.ibb.co/sJtWCn5M/images-1.jpg",name:"Double Cheeseburger",price:35000},
-  {id:5,img:"https://i.ibb.co/RkMYJJG8/44d00abe-766c-4b92-aedb-4840c48637bb.jpg",name:"Pepsi",price:10000}
+  { id: 1, img: "https://i.ibb.co/sJtWCn5M/images-1.jpg", name: "Burger", price: 50000 },
+  { id: 2, img: "https://i.ibb.co/sJtWCn5M/images-1.jpg", name: "Cheeseburger", price: 42000 },
+  { id: 3, img: "https://i.ibb.co/sJtWCn5M/images-1.jpg", name: "Hamburger", price: 45000 },
+  { id: 4, img: "https://i.ibb.co/sJtWCn5M/images-1.jpg", name: "Double Cheeseburger", price: 35000 },
+  { id: 5, img: "https://i.ibb.co/RkMYJJG8/44d00abe-766c-4b92-aedb-4840c48637bb.jpg", name: "Pepsi", price: 10000 },
+  { id: 6, img: "https://i.ibb.co/q3590gwQ/cia493tenntd8rfc2s40-1.jpg", name: "Coca-Cola", price: 10000 }
 ];
 
 const list = document.getElementById('list');
 
-/* ---------- faqat ovqatlar ro‘yxati ---------- */
-items.forEach(it=>{
+items.forEach(it => {
   const card = document.createElement('div');
   card.className = 'card';
   card.innerHTML = `
@@ -22,17 +22,15 @@ items.forEach(it=>{
   list.appendChild(card);
 });
 
-/* ---------- botga mahsulot nomini yuborish (savatsiz) ---------- */
-function sendItem(name){
-  if(window.Telegram.WebApp){
-    Telegram.WebApp.sendData(JSON.stringify({action:"select",item:name}));
-  }else{
-    alert("Tanlandi (test): "+name);
+function sendItem(name) {
+  if (window.Telegram.WebApp) {
+    Telegram.WebApp.sendData(JSON.stringify({ action: "select", item: name }));
+  } else {
+    alert("Tanlandi (test): " + name);
   }
 }
 
-/* ---------- telegramga tayyor ---------- */
-if(window.Telegram.WebApp){
+if (window.Telegram.WebApp) {
   Telegram.WebApp.ready();
   Telegram.WebApp.expand();
 }
